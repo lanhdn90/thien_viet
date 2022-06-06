5d8246e47361ce89e283f7fae996b5cb21dc4f25
lanhdn90 <lanhdn90@gmail.com>
2022-06-06 16:03:57 +0700

    icon

--- src/components/HeaderPage/HeaderPage.tsx
+++ src/components/HeaderPage/HeaderPage.tsx
@@ -1,10 +1,12 @@
-import { Col, Row } from "antd";
+import { Col, Row, Typography } from "antd";
 import * as React from "react";
 import { Link } from "react-router-dom";
+import { MdOutlineDashboardCustomize } from "react-icons/md";
 import style from "./HeaderPage.module.scss";
 export interface HeaderPageProps {}
 
 export default function HeaderPage(props: HeaderPageProps) {
+  const { Paragraph } = Typography;
   return (
     <Row>
       <Col span={6}>right</Col>
@@ -13,16 +15,28 @@ export default function HeaderPage(props: HeaderPageProps) {
         style={{ display: "flex", justifyContent: "space-between" }}
       >
         <Link to={"Programs"} className={style.link_header}>
-          Programs
+          <Row justify="center" align="middle">
+            <MdOutlineDashboardCustomize size={20} />
+            <span style={{ marginLeft: "5px" }}>Programs</span>
+          </Row>
         </Link>
         <Link to={"Products"} className={style.link_header}>
-          Products
+          <Row justify="center" align="middle">
+            <MdOutlineDashboardCustomize size={20} />
+            <span style={{ marginLeft: "5px" }}>Products</span>
+          </Row>
         </Link>
         <Link to={"Mark"} className={style.link_header}>
-          Mark
+          <Row justify="center" align="middle">
+            <MdOutlineDashboardCustomize size={20} />
+            <span style={{ marginLeft: "5px" }}>Mark</span>
+          </Row>
         </Link>
         <Link to={"Report"} className={style.link_header}>
-          Report
+          <Row justify="center" align="middle">
+            <MdOutlineDashboardCustomize size={20} />
+            <span style={{ marginLeft: "5px" }}>Report</span>
+          </Row>
         </Link>
       </Col>
       <Col span={6}>left</Col>
